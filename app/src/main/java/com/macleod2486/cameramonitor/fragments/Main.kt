b6ebb9bb0129b24b00887dc5b3c710c58a4a2cb8
macleod2486 <macleod2486@gmail.com>
2019-09-25 01:50:40 -0500

    Put in license and added basic video view

--- app/src/main/java/com/macleod2486/cameramonitor/fragments/Main.kt
+++ app/src/main/java/com/macleod2486/cameramonitor/fragments/Main.kt
@@ -1,9 +1,33 @@
+/*
+ *
+ *   CameraMonitor
+ *    a simple application to view cameras that you have configured
+ *
+ *    Copyright (C) 2019  Manuel Gonzales Jr.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see [http://www.gnu.org/licenses/].
+ *
+ */
+
 package com.macleod2486.cameramonitor.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import com.macleod2486.cameramonitor.R
 
@@ -11,8 +35,13 @@ class Main : Fragment()
 {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-        var view = inflater.inflate(R.layout.main, container, false);
+        val view = inflater.inflate(R.layout.main, container, false)
 
-        return view;
+        val videoUri = Uri.parse("https://www.youtube.com/watch?v=V1m_qTEwy_M")
+        val videoView = view.findViewById(R.id.video) as VideoView
+        videoView.setVideoURI(videoUri)
+        videoView.start()
+
+        return view
     }
 }
